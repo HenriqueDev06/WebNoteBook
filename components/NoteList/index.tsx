f705e8215b917bf7cdf80236bd381c9991419415
HenriqueDev06 <johanhenrique18@gmail.com>
2022-03-12 22:39:43 -0300

    2 files modified

--- components/NoteList/index.tsx
+++ components/NoteList/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import * as S from './styles';
 
-import SearchBar from '../SearchBar'
+import SearchBar from '../SearchBar';
 
 const NoteList = () => {
   return (
--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import * as S from './styles';
-
 import { BiSearch } from 'react-icons/bi';
 
 const SearchBar = () => {
