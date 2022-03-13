57571fe73e1845ac0007ba5de8a44ffd5f18f3fa
HenriqueDev06 <johanhenrique18@gmail.com>
2022-03-12 22:35:07 -0300

    4 files modified

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import * as S from './styles';
 
 import NoteList from '../../components/NoteList';
+import NoteEditor from '../../components/NoteEditor';
 
 const Home = () => {
   return (
     <S.Container>
       <NoteList />
+      <NoteEditor />
     </S.Container>
   );
 };
--- pages/Home/styles.ts
+++ pages/Home/styles.ts
@@ -4,4 +4,7 @@ export const Container = styled.section`
   width: 100vw;
   height: 100vh;
   background: red;
+
+  display: grid;
+  grid-template-columns: 1fr 4fr;
 `;
