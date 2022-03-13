4513217a1062a07235ea975a84aae3a665cdeb4c
HenriqueDev06 <johanhenrique18@gmail.com>
2022-03-13 19:35:06 -0300

    2 files modified

--- components/StudentInfo/index.tsx
+++ components/StudentInfo/index.tsx
@@ -4,8 +4,18 @@ import * as S from './styles';
 
 const StudentInfo = () => {
   return (
-    <S.Container>
-      <p>teste</p>
+    <S.Container className="display-flex">
+      <div className="student display-flex">
+        <img src="https://avatars.githubusercontent.com/u/100008306?v=4" />
+        <div className="info display-flex">
+          <span className="name">Johan Henrique</span>
+          <span className="email">johanhenrique18@gmail.com</span>
+        </div>
+      </div>
+      <div className="actions display-flex">
+        <button>Theme</button>
+        <button className="exit">Sair</button>
+      </div>
     </S.Container>
   );
 };
--- components/StudentInfo/styles.ts
+++ components/StudentInfo/styles.ts
@@ -1,7 +1,54 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-  height: 90px;
+  height: 75px;
   width: 100%;
-  background: purple;
+  background: #8ed1fc;
+
+  justify-content: space-between !important;
+  padding: 0px 20px;
+
+  border-bottom: 3px solid white;
+
+  .student{
+
+    justify-content: space-between !important;
+
+    img{
+      height: 45px;
+      width: 45px;
+
+      object-fit: cover;
+      border-radius: 50%;
+    }
+
+    .info{
+      flex-direction: column;
+      padding: 0px 10px;
+
+      span{
+        width: 100%;
+        font-size: 0.8rem;
+      }
+
+      .name{
+        font-weight: 700;
+      }
+    }
+  }
+
+  .actions{
+    .exit{
+      background: #ff4938;
+      padding: 6px 10px;
+      width: 120px;
+      color: white;
+      border-radius: 5px;
+    }
+
+    button{
+      margin: 0px 10px;
+      background: transparent;
+    }
+  }
 `;
