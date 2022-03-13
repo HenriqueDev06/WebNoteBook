9eace2e3e8d3e83bc8da6a022b9a39962341e78d
HenriqueDev06 <johanhenrique18@gmail.com>
2022-03-12 22:42:53 -0300

    3 files modified

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import * as S from './styles';
-import { BiSearch } from 'react-icons/bi';
 
 const SearchBar = () => {
   return (
     <S.Container>
       <input />
-      <BiSearch />
     </S.Container>
   );
 };
