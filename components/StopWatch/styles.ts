dc44da7312775110679621c104e64878f6e010e8
HenriqueDev06 <johanhenrique18@gmail.com>
2022-03-15 02:24:23 -0300

    1 file added, 1 deleted

--- components/StopWatch/styles.ts
+++ components/StopWatch/styles.ts
@@ -1,12 +1,22 @@
 import styled from 'styled-components';
 
+import Digital from '.digital-7.ttf';
+
 export const Container = styled.section`
   background: ${(props) => props.theme.background};
 
   justify-content: space-between !important;
   padding: 0px 20px;
 
+  @font-face {
+    font-family: 'Digital';
+    src: local('Digital'), local('Digital'),
+    url(${Digital}) format('ttf');
+    font-weight: 300;
+    font-style: normal;
+}
+
   p{
-    font-family: StopwatchW00-Regular
+    font-family: Digital
   }
 `;
